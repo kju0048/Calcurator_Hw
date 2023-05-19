3098235470e1c1902607de0686f74b979f3f2937
kju <byu0408@gmail.com>
2023-05-19 15:33:54 +0900

    5/19(프로그래머 계산기 50%)

--- app/src/main/java/cal/calculator/ProgrammerCal.java
+++ app/src/main/java/cal/calculator/ProgrammerCal.java
@@ -18,6 +18,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -33,6 +34,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Stack;
+import java.lang.Integer;
 
 public class ProgrammerCal extends AppCompatActivity {
 
@@ -47,11 +49,12 @@ public class ProgrammerCal extends AppCompatActivity {
     Boolean resultSet = false; // 마지막 동작이 = 인지
 
 
-
-
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     ActionBarDrawerToggle drawerToggle;
+    RadioGroup rg;
+    RadioButton rb_hex, rb_dec, rb_oct, rb_bin;
+    int cal_mode = 1; // 0 : 16진수, 1 : 10진수, 2 : 8진수, 3 : 2진수
 
 
 
@@ -95,6 +98,60 @@ public class ProgrammerCal extends AppCompatActivity {
             }
         });
 
+        rb_hex = findViewById(R.id.bt_hex);
+        rb_dec = findViewById(R.id.bt_dec);
+        rb_oct = findViewById(R.id.bt_oct);
+        rb_bin = findViewById(R.id.bt_bin);
+
+        rg = findViewById(R.id.radioGroup);
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                if(checkedId == R.id.bt_hex){
+                    cal_mode = 0;
+                }
+                else if(checkedId == R.id.bt_dec){
+                    cal_mode = 1;
+                }
+                else if(checkedId == R.id.bt_oct){
+                    cal_mode = 2;
+                }
+                else{
+                    cal_mode = 3;
+                }
+                radioChange(cal_mode);
+            }
+        });
+    }
+
+
+    void radioChange(int mode){
+        int button_id[] = {R.id.bt_0, R.id.bt_1, R.id.bt_2, R.id.bt_3, R.id.bt_4, R.id.bt_5, R.id.bt_6, R.id.bt_7
+                        , R.id.bt_8, R.id.bt_9, R.id.bt_A, R.id.bt_B, R.id.bt_C, R.id.bt_D, R.id.bt_E, R.id.bt_F};
+        for (int i : button_id){
+            findViewById(i).setEnabled(true);
+        }
+        switch(mode){
+            case 0: // 16진수
+                //
+                break;
+            case 1: // 10진수
+                for(int i = 10; i < 16 ; i++){
+                    findViewById(button_id[i]).setEnabled(false);
+                }
+                break;
+            case 2: // 8진수
+                for(int i = 8; i < 16 ; i++){
+                    findViewById(button_id[i]).setEnabled(false);
+                }
+                break;
+            case 3: // 2진수
+                for(int i = 2; i < 16 ; i++){
+                    findViewById(button_id[i]).setEnabled(false);
+                }
+                break;
+        }
     }
 
     void init() {
@@ -108,10 +165,7 @@ public class ProgrammerCal extends AppCompatActivity {
 
     public void btClick(View v) {
         int gId = v.getId();
-        if(resultSet && gId == R.id.bt_point){
-            Toast.makeText(getApplicationContext(), ". 을 사용할 수 없습니다.", Toast.LENGTH_SHORT).show();
-            return;
-        }
+
         if (!checkList.isEmpty() && checkList.get(checkList.size() - 1) == -1) {
             tv_Expression.setText(tv_Result.getText().toString());
             checkList.clear();
@@ -132,7 +186,12 @@ public class ProgrammerCal extends AppCompatActivity {
         else if (gId == R.id.bt_8) addNumber("8");
         else if (gId == R.id.bt_9) addNumber("9");
         else if (gId == R.id.bt_0) addNumber("0");
-        else if (gId == R.id.bt_point) addPoint(".");
+        else if (gId == R.id.bt_A) addNumber("A");
+        else if (gId == R.id.bt_B) addNumber("B");
+        else if (gId == R.id.bt_C) addNumber("C");
+        else if (gId == R.id.bt_D) addNumber("D");
+        else if (gId == R.id.bt_E) addNumber("E");
+        else if (gId == R.id.bt_F) addNumber("F");
         else if (gId == R.id.bt_div) addOperator("/");
         else if (gId == R.id.bt_mul) addOperator("X");
         else if (gId == R.id.bt_add) addOperator("+");
@@ -167,29 +226,6 @@ public class ProgrammerCal extends AppCompatActivity {
     // 숫자 버튼
     void addNumber (String str){
         checkList.add(1); // 숫자가 들어왔는지 체크리스트에 표시
-        tv_Expression.append(str); // UI
-        resultSet = false;
-    }
-
-    void addPoint (String str){
-        if (checkList.isEmpty()) {
-            Toast.makeText(getApplicationContext(), ". 을 사용할 수 없습니다.", Toast.LENGTH_SHORT).show();
-            return;
-        } else if (checkList.get(checkList.size() - 1) != 1) {
-            Toast.makeText(getApplicationContext(), ". 을 사용할 수 없습니다.", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        // 하나의 수에 . 이 여러 개 오는 것을 막기
-        for (int i = checkList.size() - 2; i >= 0; i--) {
-            int check = checkList.get(i);
-            if (check == 2) {
-                Toast.makeText(getApplicationContext(), ". 을 사용할 수 없습니다.", Toast.LENGTH_SHORT).show();
-                return;
-            }
-            if (check == 0) break;
-            if (check == 1) continue;
-        }
-        checkList.add(2);
         tv_Expression.append(str); // UI
         resultSet = false;
     }
@@ -277,7 +313,7 @@ public class ProgrammerCal extends AppCompatActivity {
 
     public void btxx(View v){
         if(tv_Result.length() != 0){
-            double dtemp = Double.parseDouble(tv_Result.getText().toString()) * Double.parseDouble(tv_Result.getText().toString());
+            int dtemp = Integer.parseInt(tv_Result.getText().toString()) * Integer.parseInt(tv_Result.getText().toString());
             tv_Expression.setText(String.valueOf(dtemp));
             tv_Result.setText("");
             checkList.add(1);
@@ -294,12 +330,8 @@ public class ProgrammerCal extends AppCompatActivity {
         List<String> li = new ArrayList<String>();
         Collections.addAll(li, ex);
         String temps = li.remove(li.size() - 1);
-        double dtemps = Double.parseDouble(temps) * Double.parseDouble(temps);
-        if (dtemps % 1 == 0){
-            li.add(String.valueOf(Integer.parseInt(temps) * Integer.parseInt(temps)));
-        } else{
-            li.add(String.valueOf(dtemps));
-        }
+        li.add(String.valueOf(Integer.parseInt(temps) * Integer.parseInt(temps)));
+
         tv_Expression.setText(TextUtils.join(" ", li));
         resultSet = false;
     }
@@ -307,7 +339,7 @@ public class ProgrammerCal extends AppCompatActivity {
 
     public void btRoot(View v){
         if(tv_Result.length() != 0){
-            double dtemp = Double.parseDouble(tv_Result.getText().toString()) * Double.parseDouble(tv_Result.getText().toString());
+            int dtemp = Integer.parseInt(tv_Result.getText().toString()) * Integer.parseInt(tv_Result.getText().toString());
             tv_Expression.setText(String.valueOf(dtemp));
             tv_Result.setText("");
             checkList.add(1);
@@ -324,12 +356,8 @@ public class ProgrammerCal extends AppCompatActivity {
         List<String> li = new ArrayList<String>();
         Collections.addAll(li, ex);
         String temps = li.remove(li.size() - 1);
-        double dtemps = Double.parseDouble(temps) * Double.parseDouble(temps);
-        if (dtemps % 1 == 0){
-            li.add(String.valueOf(Integer.parseInt(temps) * Integer.parseInt(temps)));
-        } else{
-            li.add(String.valueOf(dtemps));
-        }
+        li.add(String.valueOf(Integer.parseInt(temps) * Integer.parseInt(temps)));
+
         tv_Expression.setText(TextUtils.join(" ", li));
         resultSet = false;
     }
@@ -384,9 +412,13 @@ public class ProgrammerCal extends AppCompatActivity {
     boolean isNumber (String str){
         boolean result = true;
         try {
-            Double.parseDouble(str);
+            Integer.parseInt(str);
         } catch (NumberFormatException e) {
-            result = false;
+            try{
+                Integer.parseInt(str, 16);
+            } catch (NumberFormatException e2){
+                result = false;
+            }
         }
         return result;
     }
@@ -395,7 +427,9 @@ public class ProgrammerCal extends AppCompatActivity {
     void infixToPostfix () {
         for (String item : infixList) {
             // 피연산자
-            if (isNumber(item)) postfixList.add(String.valueOf(Double.parseDouble(item)));
+            if (isNumber(item)) {
+                postfixList.add(item);
+            }
                 // 연산자
             else { // 5 + 3 * sqrt(56) +
                 if (operatorStack.isEmpty()) operatorStack.push(item);
@@ -411,9 +445,26 @@ public class ProgrammerCal extends AppCompatActivity {
 
     // 계산
     String calculate (String num1, String num2, String op){
-        double first = Double.parseDouble(num1);
-        double second = Double.parseDouble(num2);
-        double result = 0.0;
+        int first = 0, second = 0, result = 0;
+        switch(cal_mode){
+            case 0:
+                first = Integer.parseInt(num1, 16);
+                second = Integer.parseInt(num2, 16);
+                break;
+            case 1:
+                first = Integer.parseInt(num1);
+                second = Integer.parseInt(num2);
+                break;
+            case 2:
+                first = Integer.parseInt(num1, 8);
+                second = Integer.parseInt(num2, 8);
+                break;
+            case 3:
+                first = Integer.parseInt(num1, 2);
+                second = Integer.parseInt(num2, 2);
+                break;
+        }
+
         try {
             switch (op) {
                 case "X":
@@ -435,7 +486,18 @@ public class ProgrammerCal extends AppCompatActivity {
         } catch (Exception e) {
             Toast.makeText(getApplicationContext(), "연산할 수 없습니다.", Toast.LENGTH_SHORT).show();
         }
-        return String.valueOf(result);
+
+        switch(cal_mode){
+            case 0:
+                return Integer.toHexString(result);
+            case 1:
+                return String.valueOf(result);
+            case 2:
+                return Integer.toOctalString(result);
+            case 3:
+                return Integer.toBinaryString(result);
+        }
+        return "";
     }
 
     // 최종 결과
@@ -449,16 +511,31 @@ public class ProgrammerCal extends AppCompatActivity {
             }
             i++;
         }
-        double temp = Double.parseDouble(postfixList.remove(0));
-        temp = Double.valueOf(Math.round(temp * 100000) / 100000.0);
-        if (temp % 1 == 0){
-            tv_Result.setText(Integer.toString((int)temp));
-        } else{
-            tv_Result.setText(Double.toString(temp));
+        String temp = postfixList.remove(0);
+        int dec_temp = 0;
+        switch(cal_mode){
+            case 0:
+                dec_temp = Integer.parseInt(temp, 16);
+                break;
+            case 1:
+                dec_temp = Integer.parseInt(temp);
+                break;
+            case 2:
+                dec_temp = Integer.parseInt(temp, 8);
+                break;
+            case 3:
+                dec_temp = Integer.parseInt(temp, 2);
+                break;
+            default:
+                dec_temp = 0;
         }
+        tv_Result.setText(temp);
+        rb_hex.setText("HEX\t\t\t" + Integer.toHexString(dec_temp));
+        rb_dec.setText("DEC\t\t\t" + Integer.valueOf(dec_temp));
+        rb_oct.setText("OCT\t\t\t" + Integer.toOctalString(dec_temp));
+        rb_bin.setText("BIN\t\t\t" + Integer.toBinaryString(dec_temp));
         //tv_Result.setText(postfixList.remove(0));
         infixList.clear();
         resultSet = true;
     }
-
 }
